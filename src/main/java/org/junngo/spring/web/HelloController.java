3427ce58194919344a691e5bc10e307f07ff4bb8
Myeongjun Ko <mjunko@dozn.co.kr>
2021-03-22 10:43:55 +0900

    added jar file

--- src/main/java/org/junngo/spring/web/HelloController.java
+++ src/main/java/org/junngo/spring/web/HelloController.java
@@ -8,10 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import javax.xml.soap.MessageFactory;
+import javax.xml.soap.MimeHeaders;
+import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPMessage;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.stream.Collectors;
 
@@ -24,37 +25,47 @@ public class HelloController {
     }
 
     @PostMapping("/callback")
-    public String callback(HttpServletRequest request) throws IOException {
+    public String callback(HttpServletRequest request) throws IOException, SOAPException {
         String test;
 //        test = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
 //        System.out.println(test);
 
-        InputStream is = request.getInputStream();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-
-        StringBuilder sb = new StringBuilder();
-        String line = null;
-        try
-        {
-            while ((line = reader.readLine()) != null)
-            {
-                sb.append(line + "\n");
-            }
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-        finally {
-            try {
-                is.close();
-            }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-            }
-        }
-        System.out.println(sb.toString());
+//        InputStream is = request.getInputStream();
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+//
+//        StringBuilder sb = new StringBuilder();
+//        String line = null;
+//        try
+//        {
+//            while ((line = reader.readLine()) != null)
+//            {
+//                sb.append(line + "\n");
+//            }
+//        }
+//        catch (IOException e)
+//        {
+//            e.printStackTrace();
+//        }
+//        finally {
+//            try {
+//                is.close();
+//            }
+//            catch (IOException e)
+//            {
+//                e.printStackTrace();
+//            }
+//        }
+//        System.out.println(sb.toString());
+        MessageFactory messageFactory = MessageFactory.newInstance();
+        InputStream inStream = request.getInputStream();
+        SOAPMessage soapMessage = messageFactory.createMessage(new MimeHeaders(), inStream);
+//        PrintWriter writer = response.getWriter();
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        soapMessage.writeTo(out);
+        String strMsg = new String(out.toByteArray());
+        System.out.println(strMsg);
+        System.out.println("========================");
+//        writer.println(strMsg);
 
         return "hello";
 
